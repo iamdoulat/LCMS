e7e49ce700e6ed99b97eb9acb37933683333ddd1
SMS WAY APP <smswayapp@gmail.com>
2025-06-22 18:22:52 +0000

    FIREBASE_ADMIN_PROJECT_ID=lc-vision FIREBASE_ADMIN_CLIENT_EMAIL=firebase

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -14,7 +14,6 @@ if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
-    console.log("Firebase Admin SDK initialized.");
   } catch (error: any) {
     console.error("Firebase Admin SDK initialization error: ", error.stack);
   }
