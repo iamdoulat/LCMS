fb81205636ff07e7e6c6f58cca2e46c5bf93c2cd
SMS WAY APP <smswayapp@gmail.com>
2025-07-17 05:08:09 +0000

    The `npm install` command failed in my project. Analyze the following er

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+    webpack: (config, { isServer }) => {
+    // This is to polyfill some node specific modules that are not available in the browser
+    // and are dependencies of easyinvoice.
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false, // You can provide a mock here if needed, or just an empty module
+        path: false,
+        os: false,
+        net: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
