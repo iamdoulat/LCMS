a3e04b6d8261003c2a9a8ef5a20916eab1c4f8a8
Smart Ideas <smartideasbd24@gmail.com>
2025-08-02 16:07:22 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TableCaption clas

--- src/components/dashboard/SalesInvoiceList.tsx
+++ src/components/dashboard/SalesInvoiceList.tsx
@@ -423,7 +423,7 @@ export function SalesInvoiceList({ showFilters = true, itemsPerPage = DEFAULT_IT
             )}
             </TableBody>
             <TableCaption className="py-4">
-            A list of your sales invoices. Showing {currentItems.length > 0 ? indexOfFirstItem + 1 : 0}-{Math.min(indexOfLastItem, displayedInvoices.length)} of {displayedInvoices.length} entries.
+            A list of your Sales Invoices from Database. Showing {currentItems.length > 0 ? indexOfFirstItem + 1 : 0}-{Math.min(indexOfLastItem, displayedInvoices.length)} of {displayedInvoices.length} entries.
             </TableCaption>
         </Table>
         </div>
