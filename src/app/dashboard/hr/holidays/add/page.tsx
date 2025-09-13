d4b6292e3d59191f5172afd4892dada6710b9683
SMS WAY APP <smswayapp@gmail.com>
2025-09-13 09:49:49 +0000

    Width will be full page form align in middle. (_for element main_)

--- src/app/dashboard/hr/holidays/add/page.tsx
+++ src/app/dashboard/hr/holidays/add/page.tsx
@@ -22,7 +22,7 @@ export default function AddHolidayPage() {
           </Button>
         </Link>
       </div>
-      <Card className="max-w-2xl mx-auto shadow-xl">
+      <Card className="mx-auto shadow-xl">
         <CardHeader>
           <CardTitle className={cn("font-bold text-2xl lg:text-3xl flex items-center gap-2", "bg-gradient-to-r from-[hsl(var(--primary))] via-[hsl(var(--accent))] to-rose-500 text-transparent bg-clip-text hover:tracking-wider transition-all duration-300 ease-in-out")}>
             <CalendarPlus className="h-7 w-7 text-primary"/>
