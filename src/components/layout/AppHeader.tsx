6c19c265c54e453607857fd04ca95854cceb0683
Smart Ideas <smartideasbd24@gmail.com>
2025-10-01 17:39:51 +0000

    Rename to Indenting And LC Management System (_for element a_)

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -61,7 +61,7 @@ export function AppHeader() {
                 href="/dashboard"
                 className={cn("text-lg md:text-xl whitespace-nowrap font-bold", "bg-gradient-to-r from-[hsl(var(--primary))] via-[hsl(var(--accent))] to-rose-500 text-transparent bg-clip-text hover:tracking-wider transition-all duration-300 ease-in-out")}
                 >
-                LC Management System
+                Indenting And LC Management System
             </Link>
         </div>
 
