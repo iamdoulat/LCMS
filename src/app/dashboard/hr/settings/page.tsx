26dbfd91a32f65f34b8c2c3aad7c58ab0199d356
Smart Ideas <smartideasbd24@gmail.com>
2025-10-06 17:08:59 +0000

    card width will be increase to full screen. add this functionality to ma

--- src/app/dashboard/hr/settings/page.tsx
+++ src/app/dashboard/hr/settings/page.tsx
@@ -167,7 +167,7 @@ export default function HrmSettingsPage() {
 
     return (
         <div className="container mx-auto py-8 px-5">
-            <Card className="max-w-7xl mx-auto shadow-xl">
+            <Card className="shadow-xl">
                 <CardHeader>
                     <CardTitle className={cn("font-bold text-2xl lg:text-3xl flex items-center gap-2 text-primary", "bg-gradient-to-r from-[hsl(var(--primary))] via-[hsl(var(--accent))] to-rose-500 text-transparent bg-clip-text hover:tracking-wider transition-all duration-300 ease-in-out")}>
                         <Settings className="h-7 w-7 text-primary" />
