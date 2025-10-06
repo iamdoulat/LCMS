808b2b8b6848dc4955f214d7d70cdc9729bb68c6
Smart Ideas <smartideasbd24@gmail.com>
2025-10-06 05:35:44 +0000

    add here 20px padding only left and right side. (_for element src/app/da

--- src/app/dashboard/hr/payroll/salary-generation/page.tsx
+++ src/app/dashboard/hr/payroll/salary-generation/page.tsx
@@ -166,7 +166,7 @@ export default function HrmSettingsPage() {
     );
 
     return (
-        <div className="container mx-auto py-8">
+        <div className="container mx-auto py-8 px-5">
             <Card className="max-w-7xl mx-auto shadow-xl">
                 <CardHeader>
                     <CardTitle className={cn("font-bold text-2xl lg:text-3xl flex items-center gap-2 text-primary", "bg-gradient-to-r from-[hsl(var(--primary))] via-[hsl(var(--accent))] to-rose-500 text-transparent bg-clip-text hover:tracking-wider transition-all duration-300 ease-in-out")}>
@@ -313,3 +313,5 @@ export default function HrmSettingsPage() {
     );
 }
   
+
+    
