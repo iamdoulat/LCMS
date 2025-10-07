9dcb7ad480db4df42969983358e9e52bbad23c8e
Smart Ideas <smartideasbd24@gmail.com>
2025-10-07 10:33:06 +0000

    card width will be increase to full screen. add this functionality to ma

--- src/app/dashboard/hr/leaves/add/page.tsx
+++ src/app/dashboard/hr/leaves/add/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { AddLeaveForm } from '@/components/forms/AddLeaveForm';
@@ -22,7 +21,7 @@ export default function AddLeavePage() {
           </Button>
         </Link>
       </div>
-      <Card className="max-w-screen-2xl mx-auto shadow-xl">
+      <Card className="mx-auto shadow-xl">
         <CardHeader>
           <CardTitle className={cn("font-bold text-2xl lg:text-3xl flex items-center gap-2", "bg-gradient-to-r from-[hsl(var(--primary))] via-[hsl(var(--accent))] to-rose-500 text-transparent bg-clip-text hover:tracking-wider transition-all duration-300 ease-in-out")}>
             <Mailbox className="h-7 w-7 text-primary"/>New Leave Application
