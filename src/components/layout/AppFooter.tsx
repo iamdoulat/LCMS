163dd2dc0431034f35e66b40707b5df083da34b0
Smart Ideas <smartideasbd24@gmail.com>
2025-10-30 10:04:12 +0000

    This hyperlink will be https://vcard.mddoulat.com/iamdoulat (_for elemen

--- src/components/layout/AppFooter.tsx
+++ src/components/layout/AppFooter.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import * as React from 'react';
@@ -14,7 +15,7 @@ export function AppFooter() {
         <p>
           &copy; {currentYear} - Designed and Developed by{" "}
           <Link
-            href="https://cards.mddoulat.com/iamdoulat"
+            href="https://vcard.mddoulat.com/iamdoulat"
             target="_blank"
             rel="noopener noreferrer"
             className="text-primary hover:underline"
