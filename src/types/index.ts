b630efa7cb859ca26667df49d82598532b68a72c
Smart Ideas <smartideasbd24@gmail.com>
2025-11-30 08:42:29 +0000

    Also add SOUTH KOREA, CZECH REPUBLIC, AND OTHERS options of Certificate

--- src/types/index.ts
+++ src/types/index.ts
@@ -30,7 +30,7 @@ export const partialShipmentAllowedOptions = ["Yes", "No"] as const;
 export type PartialShipmentAllowed = typeof partialShipmentAllowedOptions[number];
 
 export const certificateOfOriginCountries = [
-  "JAPAN", "CHINA", "TAIWAN", "SINGAPORE", "VIETNAM", "MALAYSIA", "ITALY", "USA", "THAILAND", "HONG KONG", "TURKEY", "GERMANY",
+  "JAPAN", "CHINA", "TAIWAN", "SINGAPORE", "VIETNAM", "MALAYSIA", "ITALY", "USA", "THAILAND", "HONG KONG", "TURKEY", "GERMANY", "SOUTH KOREA", "CZECH REPUBLIC", "OTHERS",
 ] as const;
 export type CertificateOfOriginCountry = typeof certificateOfOriginCountries[number];
 
