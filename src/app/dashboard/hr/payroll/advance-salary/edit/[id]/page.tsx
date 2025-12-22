6bb51d1f83082761f0d77719523686bc29c9c7b6
Doulat <iamdoulat2021@gmail.com>
2025-12-22 14:44:49 +0600

    bugs fixed

--- src/app/dashboard/hr/payroll/advance-salary/edit/[id]/page.tsx
+++ src/app/dashboard/hr/payroll/advance-salary/edit/[id]/page.tsx
@@ -99,7 +99,7 @@ export default function EditAdvanceSalaryPage() {
           </Button>
         </Link>
       </div>
-      <Card className="max-w-4xl mx-auto shadow-xl">
+      <Card className="max-w-full mx-auto shadow-xl">
         <CardHeader>
           <CardTitle className={cn("font-bold text-2xl lg:text-3xl flex items-center gap-2", "bg-gradient-to-r from-[hsl(var(--primary))] via-[hsl(var(--accent))] to-rose-500 text-transparent bg-clip-text hover:tracking-wider transition-all duration-300 ease-in-out")}>
             <Edit className="h-7 w-7 text-primary" />
